5fef8af4ecb69167f90bd446f286fd93eb00baf3
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-10 21:49:24 +0530

    power of two

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicPrograming1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result;
+            Console.WriteLine("Enter The Power");
+            int Power = int.Parse(Console.ReadLine());
+            int a = 2;
+            for (int i = 0; i <= Power; i++)
+            {
+                result = Math.Pow(a, i);
+                Console.WriteLine(a + " power of " + i + " is " + result);
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
